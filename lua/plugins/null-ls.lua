a57d487c1dd80c312c20a505862ac45c55010c65
Ian Gong <emailforgong@gmail.com>
2023-09-19 03:34:21 +0800

    feat: added markdown preview
    added markdown preview, markdown lsp and formatting

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -6,6 +6,12 @@ return {
         opts.sources = {
             nls.builtins.formatting.stylua,
             nls.builtins.diagnostics.codespell,
+            nls.builtins.formatting.deno_fmt,
+            nls.builtins.formatting.deno_fmt.with({
+                -- only with markdown for now, it also works
+                -- with .ts, .js and json
+                filetypes = { "markdown" },
+            }),
         }
     end,
 }
