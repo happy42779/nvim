e38cdd94548d2687b3c7fe332223eac3382854c6
Ian <emailforgong@gmail.com>
2023-09-20 15:49:47 +0800

    feat: clangd switch header
    enable it here, and changing it to another keymap, as Alt+o is used for editing

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,31 +1,45 @@
 return {
-    "neovim/nvim-lspconfig",
-    init = function()
-        require("lazyvim.util").on_attach(function(_, buffer)
-            vim.keymap.set("n", "<space>f", function()
-                vim.lsp.buf.format({ async = true })
-            end, { desc = "Format Document", buffer = buffer })
-        end)
-    end,
-    opts = {
-        inlay_hints = {
-            enabled = true,
-        },
-        servers = {
-            lua_ls = {
-                settings = {
-                    Lua = {
-                        hint = {
-                            enable = true,
+    {
+        "neovim/nvim-lspconfig",
+        init = function()
+            require("lazyvim.util").on_attach(function(_, buffer)
+                vim.keymap.set("n", "<space>f", function()
+                    vim.lsp.buf.format({ async = true })
+                end, { desc = "Format Document", buffer = buffer })
+            end)
+        end,
+        opts = {
+            inlay_hints = {
+                enabled = true,
+            },
+            servers = {
+                jsonls = {},
+                bashls = {},
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            hint = {
+                                enable = true,
+                            },
+                        },
+                    },
+                },
+                clangd = {
+                    keys = {
+                        {
+
+                            "<space>o",
+                            "<Cmd>ClangdSwitchSourceHeader<CR>",
+                            desc = "Switch to Source/Header",
                         },
                     },
                 },
             },
-        },
-        setup = {
-            clangd = function(_, opts)
-                opts.capabilities.offsetEncoding = { "utf-16" }
-            end,
+            setup = {
+                clangd = function(_, opts)
+                    opts.capabilities.offsetEncoding = { "utf-16" }
+                end,
+            },
         },
     },
 }
