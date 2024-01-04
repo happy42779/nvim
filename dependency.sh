d04dc071ac69dbce41722d6247c611de061d39ab
Ian <emailforgong@gmail.com>
2024-01-04 05:32:57 -0500

    Add: added system tool dependencies that will be used

---
+++ dependency.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install unzip
+sudo apt-get install git
+sudo apt-get install curl
+sudo apt-get install ripgrep
+sudo apt-get install fd-find
+sudo apt-get install fzf
+sudo apt-get install lazygit
